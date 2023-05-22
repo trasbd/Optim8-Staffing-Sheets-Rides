a2821d234a7d36c529bd21451fa42c6c53c74a97
Thomas Robert <thomas.m.robert@gmail.com>
2023-05-21 20:50:33 -0500

    added branch and new class

---
+++ Optim8 Staffing Sheets/parseHTML.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optim8_Staffing_Sheets
+{
+    internal class parseHTML
+    {
+    }
+}
