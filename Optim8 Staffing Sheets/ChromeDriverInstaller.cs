c91aff9beeda1f0400a5593d1a76de9fe5f3f717
Thomas Robert <thomas.m.robert@gmail.com>
2023-09-26 16:28:53 -0500

    fixed chromedriver updater for new chromedriver website

--- Optim8 Staffing Sheets/ChromeDriverInstaller.cs
+++ Optim8 Staffing Sheets/ChromeDriverInstaller.cs
@@ -14,7 +14,8 @@ public class ChromeDriverInstaller
 {
     private static readonly HttpClient httpClient = new HttpClient
     {
-        BaseAddress = new Uri("https://chromedriver.storage.googleapis.com/")
+        //BaseAddress = new Uri("https://chromedriver.storage.googleapis.com/")
+        BaseAddress = new Uri("https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/")
     };
 
     public Task Install() => Install(null, false);
@@ -31,11 +32,12 @@ public class ChromeDriverInstaller
         }
 
         //   Take the Chrome version number, remove the last part, 
-        chromeVersion = chromeVersion.Substring(0, chromeVersion.LastIndexOf('.'));
+        // chromeVersion = chromeVersion.Substring(0, chromeVersion.LastIndexOf('.'));
 
         //   and append the result to URL "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_". 
         //   For example, with Chrome version 72.0.3626.81, you'd get a URL "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_72.0.3626".
-        var chromeDriverVersionResponse = await httpClient.GetAsync($"LATEST_RELEASE_{chromeVersion}");
+        // 117.0.5938.92/win64/chromedriver-win64.zip
+        var chromeDriverVersionResponse = await httpClient.GetAsync($"{chromeVersion}/win64/chromedriver-win64.zip");
         if (!chromeDriverVersionResponse.IsSuccessStatusCode)
         {
             if (chromeDriverVersionResponse.StatusCode == HttpStatusCode.NotFound)
@@ -54,18 +56,8 @@ public class ChromeDriverInstaller
         string driverName;
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
         {
-            zipName = "chromedriver_win32.zip";
+            zipName = "chromedriver-win64.zip";
             driverName = "chromedriver.exe";
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-            zipName = "chromedriver_linux64.zip";
-            driverName = "chromedriver";
-        }
-        else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-        {
-            zipName = "chromedriver_mac64.zip";
-            driverName = "chromedriver";
         }
         else
         {
@@ -108,7 +100,7 @@ public class ChromeDriverInstaller
 
         //   Use the URL created in the last step to retrieve a small file containing the version of ChromeDriver to use. For example, the above URL will get your a file containing "72.0.3626.69". (The actual number may change in the future, of course.)
         //   Use the version number retrieved from the previous step to construct the URL to download ChromeDriver. With version 72.0.3626.69, the URL would be "https://chromedriver.storage.googleapis.com/index.html?path=72.0.3626.69/".
-        var driverZipResponse = await httpClient.GetAsync($"{chromeDriverVersion}/{zipName}");
+        var driverZipResponse = await httpClient.GetAsync($"{chromeVersion}/win64/chromedriver-win64.zip");
         if (!driverZipResponse.IsSuccessStatusCode)
         {
             throw new Exception($"ChromeDriver download request failed with status code: {driverZipResponse.StatusCode}, reason phrase: {driverZipResponse.ReasonPhrase}");
@@ -120,10 +112,11 @@ public class ChromeDriverInstaller
         using (var zipArchive = new ZipArchive(zipFileStream, ZipArchiveMode.Read))
         using (var chromeDriverWriter = new FileStream(targetPath, FileMode.Create))
         {
-            var entry = zipArchive.GetEntry(driverName);
-            MessageBox.Show(driverName + " installed!");
+            var entry = zipArchive.GetEntry("chromedriver-win64/" + driverName);
+            
             Stream chromeDriverStream = entry.Open();
             await chromeDriverStream.CopyToAsync(chromeDriverWriter);
+            MessageBox.Show(driverName + " installed!");
         }
 
         // on Linux/macOS, you need to add the executable permission (+x) to allow the execution of the chromedriver
